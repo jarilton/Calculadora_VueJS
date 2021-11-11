b721b49b5978b6cdece4a39708a1c54af47b8521
jarilton <jariltonpereira02@hotmail.com>
2021-11-11 19:13:18 -0300

    Criando componentes

---
+++ src/main/Calculator.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="calculator">
+        <Display />
+        <Button />
+        <Button />
+        <Button />
+    </div>
+</template>
+
+<script>
+import Display from "../components/Display.vue"
+import Button from "../components/Button.vue"
+
+export default {
+    components: { Button, Display}
+}
+</script>
+
+<style>
+
+.calculator {
+    height: 100px;
+    width: 235px;
+    border-radius: 5px;
+    overflow: hidden;
+
+    display: grid;
+    grid-template-columns: repeat(4, 25%);
+    grid-auto-rows: 1fr 48px 48px 48px 48px 48px;
+}
+
+</style>
